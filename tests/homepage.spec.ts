c729a45a517c53e03bf7e20bde4028d54edfe5f8
Danilo Vasojević <danilo.m.vasojevic@gmail.com>
2024-01-14 04:05:59 +0100

    Simple fixture instead of before / after hooks, extended test info

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,42 +1,16 @@
-import { test, expect } from '@playwright/test'
-
-const startTime = Date.now()
+import { test, expect } from '../fixtures/fixtures'
 
 test.describe('@homepage', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/')
+  test('has title', async ({ pg }) => {
+    await expect(pg).toHaveTitle(/Playwright/)
   })
 
-  test.afterEach(async ({ page }) => {
-    await page.close()
+  test('logo looks okay', async ({ pg }) => {
+    await expect(pg.getByAltText('Playwright logo')).toHaveScreenshot('logo.png')
   })
 
-  test('time is passing', async () => {
-    test.info().annotations.push({
-      type: 'Start Time',
-      description: new Date(startTime).toString(),
-    })
-    expect(Date.now()).toBeGreaterThan(startTime)
-  })
-
-  test('has title', async ({ page }) => {
-    await expect(page).toHaveTitle(/Playwright/)
-  })
-
-  test('logo looks okay', async ({ page }) => {
-    await expect(page.getByAltText('Playwright logo')).toHaveScreenshot('logo.png')
-  })
-
-  test('get started link works', async ({ page }) => {
-    await page.getByRole('link', { name: 'Get started' }).click()
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()
-  })
-
-  test('this one fails first time only', async ({ browserName }, testInfo) => {
-    await test.step('Skip test if browser is Firefox or WebKit', async () => {
-      test.skip(browserName === 'firefox', 'Do not run on Firefox')
-      test.skip(browserName === 'webkit', 'Do not run on WebKit')
-    })
-    if (!testInfo.retry) expect(true).toEqual(false)
+  test('get started link works', async ({ pg }) => {
+    await pg.getByRole('link', { name: 'Get started' }).click()
+    await expect(pg.getByRole('heading', { name: 'Installation' })).toBeVisible()
   })
 })
