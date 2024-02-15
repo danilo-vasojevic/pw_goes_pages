cfccf11fc37a36bfd4ecf743a054c48fd2a30863
Danilo Vasojević <danilo.m.vasojevic@gmail.com>
2024-02-15 04:02:32 +0100

    PW version bump, config extended

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,32 +6,38 @@ import * as dotenv from 'dotenv'
 process.env.ENV = process.env.ENV ? process.env.ENV : 'default' // Which .env file to read
 dotenv.config({ path: `config/${process.env.ENV}.env` }) // Read .env
 export const BASE_URL = process.env.BASE_URL
-export const seconds = 1000
-export const percent = 0.01
+const DEFAULT_PERMISSIONS = process.env.DEFAULT_PERMISSIONS!.split(',')
+const seconds = 1000
+const percent = 0.01
 
 // Configuration for entire Playwright project
 // See https://playwright.dev/docs/test-configuration
 export default defineConfig({
+  maxFailures: 10, // Maximum number of test failures before stopping execution
+  timeout: 30 * seconds, // Timeout for each test in milliseconds
   testDir: './tests', // Directory where Playwright looks for tests
   snapshotPathTemplate: 'data/{testFileName}/{arg}{ext}', // Where to generate / look for snapshots
   fullyParallel: true, // Run tests in files in parallel
   forbidOnly: !!process.env.CI, // Fail the build on CI if you accidentally left test.only in the source code
-  retries: !!process.env.CI ? 1 : 1, // Retry on CI only
-  workers: !!process.env.CI ? 4 : 8, // Opt out of parallel tests on CI
+  retries: process.env.CI ? 1 : 1, // Retry on CI only
+  workers: process.env.CI ? 4 : 8, // Opt out of parallel tests on CI
   outputDir: 'output/', // Folder for test artifacts such as screenshots, videos, traces, etc
   reporter: !!process.env.CI // Reporter to use. See https://playwright.dev/docs/test-reporters
-    ? /* Local */ [['list'], ['blob']]
-    : /*  CI   */ [['list'], ['html', { open: 'on-failure' }]],
+    ? /*  CI   */[['list'], ['blob']]
+    : /* Local */[['list'], ['html', { open: 'on-failure' }]],
 
-  // Shared settings for all the projects below
+  // Shared settings for all the projects
   // See https://playwright.dev/docs/api/class-testoptions
   use: {
-    viewport: { width: 1920, height: 1080 },
+    acceptDownloads: true, // Accepts all downloads
+    actionTimeout: 5 * seconds, // No action can take longer than 5 seconds
     baseURL: `${BASE_URL}`, // Base URL to use in actions like `await page.goto('/')`
+    geolocation: { longitude: 19.997393, latitude: 43.275428 },
+    permissions: DEFAULT_PERMISSIONS, // Used in all projects
     headless: true, // Run tests in headless mode
-    actionTimeout: 5 * seconds, // No action can take longer than 8 seconds
     trace: 'on-first-retry', // Collect trace when retrying the failed test
     video: 'on-first-retry', // Collect video when retrying the failed test
+    viewport: { width: 1920, height: 1080 }, // Viewport resolution to use
   },
 
   // Default config for assertions
@@ -39,15 +45,34 @@ export default defineConfig({
   expect: {
     timeout: 5 * seconds, // Maximum time expect() should wait for the condition to be met
     toHaveScreenshot: {
+      animations: 'disabled', // Disables animations when comparing screenshots
+      caret: 'hide', // Hides red text caret when comparing screenshots
+      scale: 'css', //
+      threshold: 20 * percent, // How different colors can be (comparing in YIQ color space)
       maxDiffPixelRatio: 1 * percent,
+      maxDiffPixels: 1000,
     },
   },
 
   // Configure projects for major browsers
   // See https://playwright.dev/docs/api/class-testproject
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        permissions: [...new Set([ // To avoid duplication
+          ...DEFAULT_PERMISSIONS, // Combine global add new permissions
+          ...['accessibility-events', 'clipboard-read', 'clipboard-write', 'geolocation'],
+        ])]
+      },
+    },
     { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+    {
+      name: 'webkit', use: {
+        ...devices['Desktop Safari'],
+        permissions: ['geolocation'] // Override global permissions
+      }
+    },
   ],
 })
